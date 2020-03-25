1cd56fbc6ebff38f54a0ccdc9fe9829d25139182
fzlink <fazlibozatay@gmail.com>
2020-03-25 21:11:52 +0300

    V0.1

---
+++ Brushing Mad/Assets/Scripts/TeethManager.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeethManager : MonoBehaviour
+{
+    public GameObject toothPrefab;
+    public GameObject enemyPrefab;
+    public Transform rail;
+    public Vector3 pointA;
+    public Vector3 pointB;
+    private LineRenderer lineRenderer;
+    private int destroyToothStartIndex = 10;
+    private bool destroyToothStart;
+
+    int numOftoothToCreate;
+    float distance;
+    float lerpValue;
+    Vector3 instantiatePosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+        StartCoroutine(CreateRail());
+        MakeEnemyTrail();
+    }
+
+
+
+    private IEnumerator CreateRail()
+    {
+        List<GameObject> teeth = new List<GameObject>();
+        numOftoothToCreate = Mathf.RoundToInt(Vector3.Distance(pointA, pointB) / (toothPrefab.GetComponent<MeshRenderer>().bounds.extents.x * 2));
+        distance = toothPrefab.GetComponent<MeshRenderer>().bounds.extents.x/2 / numOftoothToCreate;
+        for (int i = 0; i < numOftoothToCreate; i++)
+        {
+            if(!destroyToothStart)
+                destroyToothStartIndex--;
+            if(destroyToothStartIndex == 0 || destroyToothStart)
+            {
+                destroyToothStart = true;
+                Destroy(teeth[destroyToothStartIndex++]);
+            }
+            //We increase our lerpValue
+            lerpValue += distance;
+            //Get the position
+            instantiatePosition = Vector3.Lerp(pointA, pointB, lerpValue);
+            //Instantiate the object
+            teeth.Add(Instantiate(toothPrefab, instantiatePosition,Quaternion.Euler(-90f,0f,0f), rail));
+            yield return new WaitForSeconds(0.75f);
+        }
+        print(rail.childCount);
+    }
+
+
+    private void MakeEnemyTrail()
+    {
+        //lineRenderer.alignment = LineAlignment.Local;
+        Vector3[] positions = new Vector3[21];
+        lineRenderer.GetPositions(positions);
+
+        for (int i = 0; i < positions.Length-1; i++)
+        {
+            PlaceEnemies(positions[i],positions[i+1]);
+        }
+    }
+
+    private void PlaceEnemies(Vector3 a, Vector3 b)
+    {
+        Vector3 spawnPoint;
+        for (int i = 0; i < 5; i++)
+        {
+            spawnPoint = Vector3.Lerp(a, b, i * 0.2f);
+            Instantiate(enemyPrefab, spawnPoint, Quaternion.identity);
+        }
+    }
+}
---
+++ Brushing Mad/Assets/Scripts/ToothBrush.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToothBrush : MonoBehaviour
+{
+
+    public Transform pivot;
+    public float speed;
+    public float rotSpeed;
+
+    Vector3 clampedPivotRotation;
+    float pointer_x;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        InputHandle();
+
+    }
+
+    private void Move()
+    {
+        transform.position += Vector3.forward * speed * Time.deltaTime;
+    }
+
+    private void InputHandle()
+    {
+       // float xThrow = Input.GetAxis("Horizontal") * Time.deltaTime * rotSpeed;
+       // //pivot.Rotate(Vector3.forward, xThrow);
+
+      
+        pointer_x = Input.GetAxis("Mouse X");
+        //float pointer_y = Input.GetAxis("Mouse Y");
+        if (Input.touchCount > 0)
+        {
+            pointer_x = Input.touches[0].deltaPosition.x;
+            //pointer_y = Input.touches[0].deltaPosition.y;
+        }
+        pivot.Rotate(-Vector3.forward, pointer_x * rotSpeed * Time.deltaTime);
+
+        ClampRotation();
+    }
+
+    private void ClampRotation()
+    {
+        clampedPivotRotation = pivot.eulerAngles;
+        clampedPivotRotation.z = ClampAngle(clampedPivotRotation.z, -90f, 90f);
+        pivot.rotation = Quaternion.Euler(clampedPivotRotation);
+    }
+
+    float ClampAngle(float angle, float from, float to)
+    {
+        if (angle < 0f) angle = 360 + angle;
+        if (angle > 180f) return Mathf.Max(angle, 360 + from);
+        return Mathf.Min(angle, to);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //other.transform.position = Vector3.MoveTowards(other.transform.position, other.transform.position + new Vector3(0, 20, 0), 2000);
+        Destroy(other.gameObject);
+    }
+
+}
